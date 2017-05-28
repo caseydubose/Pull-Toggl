18a228a5f8eb32488f8ca039dc479a40d8c3fc41
杜凯希 <cdube85@gmail.com>
2017-05-28 13:59:17 -0700

    Cleaning Up Data
    Enabled browser, cleaned up naming convetion, ready for testing.

--- toggl_timesheet.py
+++ toggl_timesheet.py
@@ -2,6 +2,13 @@ import requests
 from splinter import Browser
 import time
 
+'''
+For this to work, you need to setup Toggl to have the name of project exactly as it is listed in Salesforce. 
+For Admin Work, you need to list Thomson Reuters as the client and setup the respective tasks.
+You'll also need to pull the API and Workspace ID from Toggl and create a cred file.
+Finally, you will need to specifically update the field for data timeline to get the values that you want to upload. 
+'''
+
 #import Creds for Toggl & SF
 timeentry = str('https://na31.salesforce.com/a0M/e')
 
@@ -20,12 +27,12 @@ since = str("2017-04-01")
 until = str("2017-04-24")
 
 # #initialize browser
-# browser = Browser('chrome')
+ browser = Browser('chrome')
 
-# #login to SF
-# browser.visit('https://login.salesforce.com/')
-# browser.find_by_id('password').first.fill(SF_credlines[1])
-# browser.find_by_id('username').first.fill(SF_credlines[0])
+#login to SF
+ browser.visit('https://login.salesforce.com/')
+ browser.find_by_id('password').first.fill(SF_credlines[1])
+ browser.find_by_id('username').first.fill(SF_credlines[0])
 
 #pull toggl data
 r = requests.get('https://toggl.com/reports/api/v2/details', auth=(_api_token, 'api_token'), params={
@@ -49,6 +56,7 @@ for id in data['data']:
     task = id['task']
     date = id['start']
     print(date + user + client + project + str(task) + description + str(billable))
+#load into salesforce
     browser.visit(timeentry)
     browser.find_by_id("CF00N37000006A1dD").first.fill(project)
     browser.find_by_id('00N37000006A1dI').first.fill(date)
@@ -57,6 +65,7 @@ for id in data['data']:
     browser.find_by_id('CF00N37000006ADRZ').first.fill(user)
     browser.find_by_id('00N37000006A1dc').first.fill(description)
     browser.find_by_id('00N37000006A1dS').first.click()
+#decision tree for the service categories
     if billable == str("True"):
         browser.find_by_id('00N37000006A1dS').select("Billable")
         # browser.find_by_name('save').first.click()
@@ -65,7 +74,7 @@ for id in data['data']:
         browser.find_by_id('00N37000006A1dX').select(task)
         #browser.find_by_name('save').first.click()
     else:
-        browser.find_by_id('00N37000006A1dS').select(str("Operations"))
+        browser.find_by_id('00N37000006A1dS').select("Operations")
         browser.find_by_id('00N37000006A1dX').select("Client Travel")
         # browser.find_by_name('save').first.click()
     time.sleep(1)
@@ -75,14 +84,3 @@ for id in data['data']:
 
 
 
-
-
-
-#  for project in data['data']:
-#     for i in project['items']:
-#         time_entry = i['title']['time_entry']
-#         SOW = project['title']['project']
-#         client = project['title']['client']
-#         time = i['time']
-#         hours = (time / (1000 * 60 * 60)) % 24
-#         print(client + " " + SOW + " " + time_entry + ' ' + str(hours))
